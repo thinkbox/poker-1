d3cfbda6fbcea72438d6de081421742f43e2366d
Alan Gan <alanrgan@gmail.com>
2012-08-31 13:57:25 -0400

    Update setup file
    db fix and query addition

--- poker/setup.php
+++ poker/setup.php
@@ -1,17 +1,17 @@
 <?php
 
-$con = mysql_connect("localhost","root","root123");
+$con = mysql_connect("mysql0.db.koding.com","alanrgan_2d4098c","penguins666");
 if (!$con)
   {
   die('Could not connect: ' . mysql_error());
 }
-mysql_select_db("alanrgan", $con);
+mysql_select_db("alanrgan_2d4098c", $con);
 
 $deck = $_POST['deck'];
 $hand = $_POST['hand'];
 $name = $_POST['name'];
 
-mysql_query("UPDATE game_table SET deck='$deck', player1hand='$hand' WHERE player1='$name'");
+mysql_query("UPDATE game_table SET deck='$deck', player1hand='$hand', turn='1', player1money='5000' WHERE player1='$name'");
 echo "success";
 
 mysql_close($con);
