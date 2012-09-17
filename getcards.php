9978a8f45b931aa2606937044431ced7b95a030b
alanrgan <alanrgan@cl0.beta.service.aws.koding.com>
2012-09-17 09:56:31 -0700

    add back all files

---
+++ getcards.php
@@ -0,0 +1,26 @@
+<?php
+
+$con = mysql_connect("localhost","root","root123");
+if (!$con)
+  {
+  die('Could not connect: ' . mysql_error());
+}
+mysql_select_db("alanrgan", $con);
+
+$name = $_POST['name'];
+
+if($name != "") {
+    $getdeck = mysql_query("SELECT * FROM game_table WHERE player2='$name'") or die(mysql_error());
+    
+    while($row = mysql_fetch_array($getdeck)) {
+        $deck = $row['deck'];
+        $opponent = $row['player1'];
+    }
+    $array = array();
+    $array["deck"] = $deck;
+    $array["opponent"] = $opponent;
+    echo json_encode($array);
+}
+
+mysql_close($con);
+?>
